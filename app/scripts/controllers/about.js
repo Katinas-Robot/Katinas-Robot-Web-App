fdf6d6e5a1d4b2e5f28e2f30387b7d53ca01c5de
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2014-11-28 20:14:15 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name katinasRobotWebAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the katinasRobotWebAppApp
+ */
+angular.module('katinasRobotWebAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name katinasRobotWebAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the katinasRobotWebAppApp
+ */
+angular.module('katinasRobotWebAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
